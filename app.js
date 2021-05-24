cbc31ab8aa89f2e7165a74b033e971f2a3572491
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-05-24 14:51:45 -0400

    refactored to use middleware

--- app.js
+++ app.js
@@ -6,13 +6,21 @@ const {
 	models: { User, Note },
 } = require("./db");
 const path = require("path");
-
+const requireToken = async function (req, res, next) {
+	try {
+		const token = req.headers.authorization;
+		const user = await User.byToken(token);
+		req.user = user;
+		next();
+	} catch (err) {
+		next(err);
+	}
+};
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
-app.get("/api/users/:id/notes", async (req, res, next) => {
+app.get("/api/users/:id/notes", requireToken, async (req, res, next) => {
 	try {
-		const user = await User.byToken(req.headers.authorization);
-		const notes = await user.getNotes();
+		const notes = await req.user.getNotes();
 		res.json(notes);
 	} catch (err) {
 		next(err);
@@ -27,9 +35,9 @@ app.post("/api/auth", async (req, res, next) => {
 	}
 });
 
-app.get("/api/auth", async (req, res, next) => {
+app.get("/api/auth", requireToken, async (req, res, next) => {
 	try {
-		res.send(await User.byToken(req.headers.authorization));
+		res.send(req.user);
 	} catch (ex) {
 		next(ex);
 	}
